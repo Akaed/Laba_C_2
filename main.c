88834dcbb034ad589d48ec7f479c3c942922c3a7
Akaed <43893798+Akaed@users.noreply.github.com>
2020-11-11 16:11:23 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <fcntl.h>
+void func1(int *ar,int size);
 
-int main()
-{
-    printf("Hello world!\n");
+int main (){
+    int l;
+    printf("Write size of arr");
+     scanf ("%i",&l);
+    int *arr1 = (int*)malloc(sizeof(int) * l);
+
+    srand(time(NULL));
+
+      func1(arr1 , l);
+    free(arr1);
     return 0;
+}
+void func1(int *ar,int size){
+      int k;
+      int i;
+    for(i=0; i< size; i++){
+        ar[i]=1+rand()%30;
+          printf("%i \t",ar[i]);
+    }
+    printf("\nVvedite k\n");
+
+    scanf ("%i",&k);
+    for (i = 0 ; i < size;i++ ){
+        if ( ar[i] == 3*k + 1 ){
+            printf("%i \t", ar[i]);
+        }
+    }
+ /////laba2ë
 }
